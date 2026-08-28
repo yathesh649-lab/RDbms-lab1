b36d9e996d8e4b8debf5ceeced0ea20b5ffbf1fa
yathesh649-lab <yathesh649@gmail.com>
2026-08-28 12:29:25 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,13 +1,7 @@
--- Create Student table
-
--- StudentID
-
--- StudentName
-
--- DOB
-
--- Gender
-
--- DepartmentID
-
--- Add constraints
+CREATE TABLE Student (
+    StudentID NUMBER(5) PRIMARY KEY,
+    StudentName VARCHAR2(20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR2(10) NOT NULL,
+    DepartmentID NUMBER(5) UNIQUE
+);
