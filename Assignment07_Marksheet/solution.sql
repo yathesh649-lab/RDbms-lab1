439831c022e9a9dc5079e2d30d0e45a16daa64a7
yathesh649-lab <yathesh649@gmail.com>
2026-08-28 12:44:13 +0530

    Update solution.sql

--- Assignment07_Marksheet/solution.sql
+++ Assignment07_Marksheet/solution.sql
@@ -1,7 +1,16 @@
--- Create Marksheet table
+CREATE TABLE Marksheet (
+    RollNo NUMBER(5),
+    Name VARCHAR2(20),
+    Department VARCHAR2(20),
+    Marks NUMBER(3) 
+  );
+INSERT INTO Marksheet VALUES (1, 'Arun', 'CSE', 85);
+INSERT INTO Marksheet VALUES (2, 'Divya', 'IT', 78);
+INSERT INTO Marksheet VALUES (3, 'Karthik', 'CSE', 92);
+INSERT INTO Marksheet VALUES (4, 'Nisha', 'ECE', 67);
+INSERT INTO Marksheet VALUES (5, 'Rahul', 'IT', 88);
 
--- Insert sample records
-
--- Display students with Marks > 80
-
--- Sort by Marks DESC
+SELECT *
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
