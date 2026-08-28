40f9be21041ad0d8f167a51ba83e58dbc82064ff
yathesh649-lab <yathesh649@gmail.com>
2026-08-28 12:52:06 +0530

    Update solution.sql

--- Assignment08_Aggregate_Functions/solution.sql
+++ Assignment08_Aggregate_Functions/solution.sql
@@ -1,11 +1,13 @@
--- Create Employee table
+CREATE TABLE Employee (
+    EmployeeID NUMBER(5) PRIMARY KEY,
+    EmployeeName VARCHAR2(30),
+    Department VARCHAR2(20),
+    Salary NUMBER(10,2)
+);
+INSERT INTO Employee VALUES (1, 'Ravi', 'CSE', 35000);
+INSERT INTO Employee VALUES (2, 'Priya', 'IT', 40000);
+INSERT INTO Employee VALUES (3, 'Kumar', 'ECE', 32000);
+INSERT INTO Employee VALUES (4, 'Meena', 'CSE', 45000);
+INSERT INTO Employee VALUES (5, 'Suresh', 'IT', 38000);
 
--- Insert records
-
--- COUNT()
-
--- MAX()
-
--- MIN()
-
--- AVG()
+SELECT * FROM Employee;
