278a7c93fce9dd53a8387b70d5e412118aaea5d9
yathesh649-lab <yathesh649@gmail.com>
2026-08-28 12:36:49 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,14 @@
--- Create Course table
+CREATE TABLE Course (
+    CourseID NUMBER(5) PRIMARY KEY,
+    CourseName VARCHAR2(30),
+    Credits NUMBER(2),
+    DepartmentID NUMBER(5)
+);
 
--- Insert three records
+INSERT INTO Course VALUES (201, 'Database Systems', 4, 101);
+INSERT INTO Course VALUES (202, 'Web Technology', 3, 102);
+INSERT INTO Course VALUES (203, 'Programming', 4, 103);
 
--- Display structure
+SELECT * FROM Course;
+
+DESCRIBE Course;
