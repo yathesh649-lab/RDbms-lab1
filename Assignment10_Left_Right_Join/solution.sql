61ed01e8b4eaedc3816298c83cad741ac635e96b
yathesh649-lab <yathesh649@gmail.com>
2026-08-29 08:28:08 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -1,9 +1,20 @@
--- Create Course
+CREATE TABLE Course (
+    CourseID NUMBER(5) PRIMARY KEY,
+    CourseName VARCHAR2(30),
+    Credits NUMBER
+  );
+INSERT INTO Course VALUES (201, 'Database Systems', 4);
+INSERT INTO Course VALUES (202, 'Data Structures', 3);
+INSERT INTO Course VALUES (203, 'Mathematics', 4);
 
--- Create Enrollment
+CREATE TABLE Enrollment (
+    EnrollmentID NUMBER(5) PRIMARY KEY,
+    StudentID NUMBER(5),
+    CourseID NUMBER(5)
+);
 
--- Insert sample records
-
--- LEFT JOIN
-
--- RIGHT JOIN
+--
+INSERT INTO Enrollment VALUES (1, 1001, 201);
+INSERT INTO Enrollment VALUES (2, 1001, 202);
+INSERT INTO Enrollment VALUES (3, 1002, 203);
+INSERT INTO Enrollment VALUES (4, 1003, 201);
